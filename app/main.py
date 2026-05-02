eff6d928ac2db52e79f916968500df2b1b871d1f
robinlindbergg <robin.lindberg2004@gmail.com>
2026-05-02 10:57:22 +0300

    add rooms endpoint

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"msg": "Heyooo Robin!!!!!!!!!!", "v": "0.1"}
+    return {"msg": "Heyooo Robin!!!!!!!!!", "v": "0.1"}
 
 
 @app.get("/items/{id}")
@@ -23,3 +23,14 @@ def get_ip(request: Request):
 def get_ip_html(request: Request):
     ip = request.client.host
     return f"<h1>Din publika IP-adress är {ip}</h1>"
+
+rooms = [
+    {"room_number": 101, "type": "single", "price": 80, "available": True},
+    {"room_number": 102, "type": "double", "price": 120, "available": True},
+    {"room_number": 201, "type": "suite", "price": 220, "available": False},
+]
+
+
+@app.get("/rooms")
+def get_rooms():
+    return rooms
