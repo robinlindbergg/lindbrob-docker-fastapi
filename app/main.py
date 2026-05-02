dc06097d30876c8b7e7c1fa720cea359f843abfc
robinlindbergg <robin.lindberg2004@gmail.com>
2026-05-02 10:24:34 +0300

    added ip endpoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return { "msg": "Heyooo Robin!!!!!!!!!!!!!!!!!", "v": "0.1" }
+    return {"msg": "Heyooo Robin!!!!!!!!!!!!!!!", "v": "0.1"}
 
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+    return {"id": item_id, "q": q}
+
+
+@app.get("/api/ip")
+def get_ip(request: Request):
+    return {"ip": request.client.host}
+
+
+@app.get("/ip", response_class=HTMLResponse)
+def get_ip_html(request: Request):
+    ip = request.client.host
+    return f"<h1>Din publika IP-adress är {ip}</h1>"
